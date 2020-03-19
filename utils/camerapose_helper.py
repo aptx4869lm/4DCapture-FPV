fc4be599e98569372aca7a012ebb9d3025a0a019
aptx4869lm <lmaptx4869@gmail.com>
2020-03-19 21:27:55 +0100

    eidit on sfm helpers

--- utils/camerapose_helper.py
+++ utils/camerapose_helper.py
@@ -1,5 +1,5 @@
 
-
+import sys
 import argparse
 import os
 import glob
@@ -12,47 +12,24 @@ import json
 import subprocess
 from multiprocessing import current_process
 
-def main():
-  pointcloud_file = './sample1/images.txt'
-  lines = [line.rstrip('\n') for line in open(pointcloud_file)]
-  lines = lines[4:]
-  # print(lines[0])
-  text_file = open('./sample1/camerapose.txt', 'w')
+def main(camera_folder):
+  camera_file =camera_folder+'images.txt'
+  lines = [line.rstrip('\n') for line in open(camera_file)]
+  lines = lines[3:]
+  print(lines[0])
+  output_file = camera_file.replace('images','camerapose')
+  text_file = open(output_file, 'w')
 
   for line in lines:
     items = line.split(' ')
     # print(line)
-    if 'png' in items[-1]:
+    if 'jpg' in items[-1]:
       text_file.write(' '+items[1]+' '+items[2]+' '+items[3]+' '+items[4]+' '+items[5]+' '+items[6]+' '+items[7]+'\n')
     # break
   text_file.close()
-    # break
-  # results_list = sorted(glob.glob(os.path.join(results_folder, '*/*.pkl')))
 
-  # for i in range(len(results_list)):
-  #   old_file = results_list[i]
-  #   new_file = results_list[i].split('/')[-2]+'.pkl'
-  #   new_file=new_file.replace('img','')
-  #   # print(new_file)
-
-  # #   old_file = openpose_list[i]
-  # #   new_file = openpose_list[i].split('/')[-1].split('_')[-1]
-    
-  # #   new_file='img'+str(i).zfill(4)+'_'+new_file
-  # #   # print(new_file)
-  #   new_file = gen_folder+new_file
-
-
-  #   command = 'mv'+' '+old_file+' '+new_file
-  #   print(command)
-  #   try:
-  #      output = subprocess.check_output(command, shell=True,
-  #                                       stderr=subprocess.STDOUT)
-  #   except subprocess.CalledProcessError as err:
-  #      return status, err.output
-       
 
 
 if __name__ == '__main__':
-    description = 'Helper script for dumping video frames.'
-    main()
+    camera_folder=sys.argv[1]
+    main(camera_folder)
--- utils/pointcloud_helper.py
+++ utils/pointcloud_helper.py
@@ -7,49 +7,26 @@ import time
 import math
 import shutil
 import json
-
+import sys
 # for multi process
 import subprocess
 from multiprocessing import current_process
 
-def main():
-  pointcloud_file = './sample1/points3D.txt'
+def main(pointcloud_folder):
+  pointcloud_file = pointcloud_folder+'/points3D.txt'
   lines = [line.rstrip('\n') for line in open(pointcloud_file)]
   lines = lines[3:]
   # print(lines[0])
-  text_file = open('./sample1/xyz.txt', 'w')
-
+  output_file = pointcloud_file.replace('points3D','xyz').replace('txt','xyz')
+  text_file = open(output_file, 'w')
+                
   for line in lines:
     items = line.split(' ')
+
     text_file.write(' '+items[1]+' '+items[2]+' '+items[3]+' '+items[4]+' '+items[5]+' '+items[6]+'\n')
   text_file.close()
-    # break
-  # results_list = sorted(glob.glob(os.path.join(results_folder, '*/*.pkl')))
-
-  # for i in range(len(results_list)):
-  #   old_file = results_list[i]
-  #   new_file = results_list[i].split('/')[-2]+'.pkl'
-  #   new_file=new_file.replace('img','')
-  #   # print(new_file)
-
-  # #   old_file = openpose_list[i]
-  # #   new_file = openpose_list[i].split('/')[-1].split('_')[-1]
-    
-  # #   new_file='img'+str(i).zfill(4)+'_'+new_file
-  # #   # print(new_file)
-  #   new_file = gen_folder+new_file
-
-
-  #   command = 'mv'+' '+old_file+' '+new_file
-  #   print(command)
-  #   try:
-  #      output = subprocess.check_output(command, shell=True,
-  #                                       stderr=subprocess.STDOUT)
-  #   except subprocess.CalledProcessError as err:
-  #      return status, err.output
-       
 
 
 if __name__ == '__main__':
-    description = 'Helper script for dumping video frames.'
-    main()
+    pointcloud_folder=sys.argv[1]
+    main(pointcloud_folder)
